a57af4104fbcea8711cb21499932a8a9579284f3
NicolasJimenezPerez <127426296+NicolasJimenezPerez@users.noreply.github.com>
2025-12-12 11:27:24 +0100

    The file to add the EMT (Bus) login keys
    This is the placeholder file and must be edited before use with your personal keys

---
+++ TransportAPIs/EmtApiLoginKeys.cs
@@ -0,0 +1,15 @@
+namespace TransportAPIs
+{
+    public partial class EmtApi
+    {
+        /// <summary>
+        /// String that defines the login keys
+        /// </summary>
+        private string loginJson = """
+                                   {"email": "[Your Email]",
+                                   "password": "[Your Password]",
+                                   "X-ClientId": "[Your X-ClientId]",
+                                   "passKey": "[Your passKey]"}
+                                   """;
+    }
+}
